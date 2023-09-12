e1bcd44ce4a7f690f597a168c4f7ddc27a5ccd8e
Dennis Thompson <djtthompson@gmail.com>
2023-09-12 16:11:39 -0700

    fix: export correct types

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { tokenize } from "./tokenize";
 import { parse } from "./parse/parse";
+import type { Token, Types } from "./types";
 
 export const build = (expression: string) => {
   return parse(tokenize(expression));
 };
 
-export { tokenize, parse };
+export { tokenize, parse, Types };
+export type { Token };
